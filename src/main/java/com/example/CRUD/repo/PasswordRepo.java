15382c63ac4adeb69bae59c297bb44ea911201ae
AmritkThapa <amritthapa.6024@gmail.com>
2024-04-11 15:28:24 +0545

    change

--- src/main/java/com/example/CRUD/repo/PasswordRepo.java
+++ src/main/java/com/example/CRUD/repo/PasswordRepo.java
@@ -2,13 +2,14 @@ package com.example.CRUD.repo;
 
 import com.example.CRUD.entity.Password;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PasswordRepo extends JpaRepository<Password,Integer> {
     Password getById(long id);
-
     String deleteById(long id);
-
     Optional<Password> findById(Long id);
+    @Query("SELECT MAX(p.id) FROM Password p")
+    Long findMaxId();
 }
